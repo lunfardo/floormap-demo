258e84b951cb4ec3961548775996454ebb90edc8
lunfalun <lunfalun@DESKTOP-M239PTC>
2022-04-19 01:21:39 +0200

    more rooms

--- src/components/molecules/Room.tsx
+++ src/components/molecules/Room.tsx
@@ -1,24 +1,38 @@
 import React from "react";
-import { Line } from "react-konva";
+import { Line, Text } from "react-konva";
 
 type RoomProps = { index: number; room: Room };
 
 const Room: React.FC<RoomProps> = ({ index, room }) => {
+  //TODO: only works for square rooms for now
+  const middlePoint: Point = {
+    x: (room.limitWalls[0].start.x + room.limitWalls[2].start.x) / 2,
+    y: (room.limitWalls[0].start.y + room.limitWalls[2].start.y) / 2,
+  };
+
   return (
-    <Line
-      key={index}
-      id={`${index}`}
-      points={room.limitWalls.flatMap((wall) => [
-        wall.start.x,
-        wall.start.y,
-        wall.end.x,
-        wall.end.y,
-      ])}
-      closed
-      stroke={room?.color ?? "red"}
-      tension={1}
-      onClick={(e) => alert(`${room.name}`)}
-    />
+    <>
+      <Text
+        x={middlePoint.x - 15}
+        y={middlePoint.y - 10}
+        text={room.name}
+        fill="yellow"
+      />
+      <Line
+        key={index}
+        id={`${index}`}
+        points={room.limitWalls.flatMap((wall) => [
+          wall.start.x,
+          wall.start.y,
+          wall.end.x,
+          wall.end.y,
+        ])}
+        closed
+        stroke={room?.color ?? "red"}
+        tension={1}
+        onClick={(e) => alert(`${room.name}`)}
+      />
+    </>
   );
 };
 
